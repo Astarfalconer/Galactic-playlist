e926ad3db4ac9bf3738cb14ab880dfa4adadcb79
Astarfalconer <125147128+Astarfalconer@users.noreply.github.com>
2023-09-11 13:24:15 +0100

    further setting up static components

---
+++ src/components/App/App.js
@@ -0,0 +1,28 @@
+import React, {useState} from 'react'
+import './App.css';
+import SearchResults from "../SearchResults/SearchResults";
+import SearchBar from "../SearchBar/SearchBar";
+import Playlist from "../Playlist/playlist";
+function App() {
+  return (
+      <>
+      <div>
+        <div>
+          <h1>Galactic Playlist</h1>
+        </div>
+        <div className='.App'>
+          <div>
+            <SearchBar/>
+          </div>
+          <div className='.Playlist'>
+          <SearchResults/>
+          <Playlist/>
+          </div>
+    
+        </div>
+      </div>
+     </>
+  );
+}
+
+export default App;
---
+++ src/components/SearchResults/SearchResults.js
@@ -0,0 +1,4 @@
+import React from 'react';
+
+import "./SearchResults.css"
+import  TrackList from "../TrackList/TrackList";

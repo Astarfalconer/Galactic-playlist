c4a8938867930b8455f081fdad105ca62b11fc81
Astarfalconer <125147128+Astarfalconer@users.noreply.github.com>
2023-09-12 18:36:25 +0100

    finished creating static components

---
+++ src/components/Playlist/playlist.js
@@ -0,0 +1,14 @@
+import React from "react";
+import TrackList from "../TrackList/TrackList";
+import "./Playlist.css";
+
+function PlayList() {
+return (
+    <div>
+        
+    </div>
+)
+};
+
+
+export default PlayList;
--- src/components/SearchResults/SearchResults.js
+++ src/components/SearchResults/SearchResults.js
@@ -1,4 +1,15 @@
-import React from 'react';
+import React from "react";
 
 import "./SearchResults.css"
 import  TrackList from "../TrackList/TrackList";
+function SearchResults() {
+    return (
+        <>
+        <div>
+            <TrackList/>
+        </div>
+        </>
+    )
+};
+
+export default SearchResults;

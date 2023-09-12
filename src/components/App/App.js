7aba91b412488d6d96ca70e235d39042320797ab
Astarfalconer <125147128+Astarfalconer@users.noreply.github.com>
2023-09-12 18:35:13 +0100

    hghghg

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,20 +1,21 @@
-import React, {useState} from 'react'
+import React, {useState} from "react"
 import './App.css';
 import SearchResults from "../SearchResults/SearchResults";
 import SearchBar from "../SearchBar/SearchBar";
 import Playlist from "../Playlist/playlist";
 function App() {
+  const [searchResults, setSearchResults] = useState([])
   return (
       <>
       <div>
         <div>
           <h1>Galactic Playlist</h1>
         </div>
-        <div className='.App'>
+        <div className='App'>
           <div>
             <SearchBar/>
           </div>
-          <div className='.Playlist'>
+          <div className='Playlist'>
           <SearchResults/>
           <Playlist/>
           </div>
